48df610b0077aafeabe7b1c990ee4eac09a7344c
zwn <gogo.gmail.com>
2017-09-06 09:17:29 +0800

    add cookies

--- headlines.py
+++ headlines.py
@@ -2,10 +2,12 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import make_response
 import json
 import urllib
 import urllib.parse
 import urllib.request
+import datetime
 
 DEFAULTS = {
     'publication': 'bbc',
@@ -37,36 +39,42 @@ RSS_FEED = {"bbc": "http://feeds.bbci.co.uk/news/rss.xml",
 # @app.route("/cnn")
 # def cnn():
 #     return get_news('cnn')
+def get_value_with_fallback(key):
+    if request.args.get(key):
+        return request.args.get(key)
+    if request.cookies.get(key):
+        return request.cookies.get(key)
 
 
 @app.route("/")
 def home():
-    publication = request.args.get('publication')
-    if not publication:
-        publication = DEFAULTS['publication']
+    # get publication
+    publication = get_value_with_fallback("publication")
     articles = get_news(publication)
 
     # get customized weather based on user input or default
-    city = request.args.get('city')
-    if not city:
-        city = DEFAULTS['city']
+    city = get_value_with_fallback("city")
     weather = get_weather(city)
 
     # get currency_from and currency_to
-    currency_from = request.args.get('currency_from')
-    if not currency_from:
-        currency_from = DEFAULTS['currency_from']
-    currency_to = request.args.get('currency_to')
-    if not currency_to:
-        currency_to = DEFAULTS['currency_to']
-    rate,currencies = get_rate(currency_from, currency_to)
-    return render_template('home.html',
-                           articles=articles,
-                           weather=weather,
-                           rate=rate,
-                           currency_from=currency_from,
-                           currency_to=currency_to,
-                           currencies=sorted(currencies))
+    currency_from = get_value_with_fallback("currency_from")
+    currency_to = get_value_with_fallback("currency_to")
+
+    # save cookies and return template
+    rate, currencies = get_rate(currency_from, currency_to)
+    response = make_response(render_template('home.html',
+                                             articles=articles,
+                                             weather=weather,
+                                             rate=rate,
+                                             currency_from=currency_from,
+                                             currency_to=currency_to,
+                                             currencies=sorted(currencies)))
+    expires = datetime.datetime.now() + datetime.timedelta(days=36)
+    response.set_cookie("publication", publication, expires=expires)
+    response.set_cookie("city", city, expires=expires)
+    response.set_cookie("currency_from", currency_from, expires=expires)
+    response.set_cookie("currency_to", currency_to, expires=expires)
+    return response
 
 
 def get_news(query):
